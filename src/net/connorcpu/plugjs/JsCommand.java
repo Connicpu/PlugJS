70c3afe88a8b9578d497b455147936a7eac892fd
Connor Hilarides <connorcpu@live.com>
2013-05-10 19:35:22 -0400

    JsCommand for PROPER registration ^.^

---
+++ src/net/connorcpu/plugjs/JsCommand.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.connorcpu.plugjs;
+
+import org.bukkit.command.CommandSender;
+
+/**
+ *
+ * @author Connor
+ */
+public interface JsCommand {
+    public boolean execute(CommandSender sender, String commandLabel, String[] args);
+}
---
+++ src/net/connorcpu/plugjs/JsCommandExecutor.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.connorcpu.plugjs;
+
+import java.util.List;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+/**
+ *
+ * @author Connor
+ */
+public class JsCommandExecutor extends Command {
+    
+    JsCommand executor;
+    
+    public JsCommandExecutor(String name, String description, String usage, List<String> aliases, JsCommand command) {
+        super(name, description, usage, aliases);
+        
+        executor = command;
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String label, String[] args) {
+        return executor.execute(sender, label, args);
+    }
+    
+}
